bd6c55e9aaec9e256fe44c7bc2d45882beafb612
AHMADKIBRAHIM <62340517+AHMADKIBRAHIM@users.noreply.github.com>
2023-11-16 01:16:04 +0200

    2nd

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/students', function (req, res) {
+  var data = [
+    {id:1, name:'crazy'},
+    {id:2, name:'mhamd'},
+    {id:3, name:'ahmad'},
+    {id:4, name:'khaled'},
+  ]
+  res.send(data)
+})
+
 app.listen(port)
